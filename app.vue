85892ba1c5a96934c1c8b1a484bcb3a62d10fc3f
Piyush <bhangalepiyush@gmail.com>
2023-08-26 21:35:05 +0530

    feat(chore): init wallet connection

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <script lang="ts" setup>
 import { Notifications, Notivue } from 'notivue'
+import { initWallet } from '@neko/hedera'
+
+await initWallet(undefined, {
+  name: 'True.',
+  description: 'journalist go brrr',
+  url: 'https://true.pycz.dev',
+})
 </script>
 
 <template>
--- pages/login.vue
+++ pages/login.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import {} from '@neko/hedera'
+
 definePageMeta({
   layout: 'login',
 })
