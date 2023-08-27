2297b484793b34c58af74adbe7f466033bd85696
Piyush <bhangalepiyush@gmail.com>
2023-08-27 17:13:28 +0530

    fix: redierct

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
 const router = useRouter()
-onMounted(() => {
-  router.push('/login')
-})
+router.push('/login')
 </script>
 
 <template>
-  <div class="h-screen flex items-center justify-center bg-zinc-900 font-mono text-white">
-    Hello, Pizza!
-  </div>
+  <div />
 </template>
 
 <style scoped>
