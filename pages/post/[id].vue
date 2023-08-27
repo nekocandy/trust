59b4000c1ef5ece6c8c9a0f4231d1340980d25f6
Piyush <bhangalepiyush@gmail.com>
2023-08-27 17:26:50 +0530

    feat: send truken

--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import MarkdownIt from 'markdown-it'
-import { getFileContents } from '@neko/hedera-sdk'
+import { donate, getFileContents } from '@neko/hedera-sdk'
 import { usePush } from 'notivue'
 
 const push = usePush()
@@ -9,6 +9,13 @@ const { params } = useRoute()
 
 const markdown = new MarkdownIt()
 const content = await getFileContents(hederaData, params.id as string)
+
+async function donateToPost() {
+  const promise = push.promise('sending TruKen to author!')
+  const donatedTxn = await donate(params.id as string)
+
+  promise.resolve(`TruKen sent to the author! | Txn ID: ${donatedTxn}`)
+}
 
 async function trustThis() {
   const promise = push.promise('storing information!')
@@ -21,13 +28,17 @@ async function trustThis() {
 <template>
   <div flex="~ col" h-full w-full gap-4>
     <div flex-1 prose v-html="markdown.render(content)" />
-    <div w-full flex items-center justify-center gap-4 pb-4>
+    <div grid grid-cols-2 w-full items-center justify-center gap-4 pb-4>
       <button rounded bg-lime px-4 py-2 font-bold tracking-wide @click="trustThis()">
         I trust this
       </button>
 
       <button rounded bg-red px-4 py-2 font-bold tracking-wide @click="trustThis()">
         False Info
+      </button>
+
+      <button col-span-2 rounded bg-indigo px-4 py-2 font-bold tracking-wide @click="donateToPost()">
+        Donate
       </button>
     </div>
   </div>
