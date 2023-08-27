da4946fb01d99fdcd5efc09771b4f45e52aabfe6
Piyush <bhangalepiyush@gmail.com>
2023-08-27 12:02:02 +0530

    feat: add basic post

---
+++ components/NewsCardExpanded.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div flex>
+    <div class="bg-[#E4C1E1]" w-full flex flex-1 px-4 py-4>
+      <div class="flex flex-col justify-between">
+        <div>Some serious news headline that might take, upto two lines?</div>
+
+        <div text-xs font-mono>
+          posted 1 day ago
+        </div>
+      </div>
+
+      <div class="bg-[#A3B28F] -mr-6 -rotate-90" mb-8 h-fit w-fit self-end rounded-t-md px-4 py-1 font-bold>
+        @hello!
+      </div>
+      <div class="h-50 w-full bg-[url(/posts/train.png)] bg-cover bg-center bg-no-repeat pl-8" bg-red />
+    </div>
+
+    <div
+      class="bg-[#443643] px-2 text-center text-3xl font-bold text-[#FEBB93]" style="writing-mode: vertical-lr;"
+    >
+      The True Daily.
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -3,13 +3,19 @@ import { getData } from '@neko/hedera-sdk'
 </script>
 
 <template>
-  <div h-full flex flex-col gap-4>
+  <div h-full flex flex-col gap-4 overflow-y-auto pb-8>
     <div flex items-center justify-around gap-4>
       <Chip title="Active users:" :value="getData('active-users').toString()" />
       <Chip title="Posts Made:" :value="getData('posts-made').toString()" />
       <Chip title="All Posts:" :value="getData('all-posts').toString()" />
       <Chip title="Active Now:" :value="getData('active-now').toString()" />
       <Chip title="Transactions:" :value="getData('txn-total').toString()" />
+    </div>
+
+    <div class="overflow-auto">
+      <div>
+        <NewsCardExpanded />
+      </div>
     </div>
   </div>
 </template>
