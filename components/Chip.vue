9411fa7051ee93bd32ea66fe40f4c785af06812c
Piyush <bhangalepiyush@gmail.com>
2023-08-27 11:24:12 +0530

    feat: chip component

---
+++ components/Chip.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineProps({
+  title: String,
+  value: String,
+})
+</script>
+
+<template>
+  <div class="bg-[#443643] text-[#FEBB93]" w-full flex items-center justify-center gap-2 rounded px-4 py-1 text-lg>
+    <span>{{ $props.title }}</span>
+    <span>{{ $props.value }}</span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
